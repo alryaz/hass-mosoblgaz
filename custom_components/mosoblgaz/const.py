1c52b28695a38c98a11897bb56cce188c37fe04f
Alexander Ryazanov <alryaz@xavux.com>
2022-01-21 14:42:11 +0300

    fix: fix device_class not being overridden on meter entities (#2)
    refactor: constants moved
    
    refactor: added alternative name in manifest

--- custom_components/mosoblgaz/const.py
+++ custom_components/mosoblgaz/const.py
@@ -1,69 +1,78 @@
 """Constants for Mosoblgaz module"""
-CONF_CONTRACTS = "contracts"
-CONF_METER_NAME = "meter_name"
-CONF_CONTRACT_NAME = "contract_name"
-CONF_METERS = "meters"
-CONF_INVOICES = "invoices"
-CONF_INVOICE_NAME = "invoice_name"
-CONF_INVERT_INVOICES = "invert_invoices"
-CONF_PRIVACY_LOGGING = "privacy_logging"
+from typing import Final
 
-DOMAIN = "mosoblgaz"
-DATA_CONFIG = DOMAIN + "_config"
-DATA_API_OBJECTS = DOMAIN + "_api_objects"
-DATA_ENTITIES = DOMAIN + "_entities"
-DATA_UPDATERS = DOMAIN + "_updaters"
-DATA_OPTIONS_LISTENERS = DOMAIN + "_options_listeners"
+CONF_CONTRACTS: Final = "contracts"
+CONF_METER_NAME: Final = "meter_name"
+CONF_CONTRACT_NAME: Final = "contract_name"
+CONF_METERS: Final = "meters"
+CONF_INVOICES: Final = "invoices"
+CONF_INVOICE_NAME: Final = "invoice_name"
+CONF_INVERT_INVOICES: Final = "invert_invoices"
+CONF_PRIVACY_LOGGING: Final = "privacy_logging"
 
-DEFAULT_SCAN_INTERVAL = 60 * 60  # 1 hour
-DEFAULT_TIMEOUT = 30  # 30 seconds
-DEFAULT_CONTRACT_NAME_FORMAT = "MOG Contract {contract_code}"
-DEFAULT_METER_NAME_FORMAT = "MOG Meter {meter_code}"
-DEFAULT_INVOICE_NAME_FORMAT = "MOG {group} Invoice {contract_code}"
-DEFAULT_INVERT_INVOICES = False
-DEFAULT_ADD_INVOICES = True
-DEFAULT_ADD_METERS = True
-DEFAULT_ADD_CONTRACTS = True
-DEFAULT_PRIVACY_LOGGING = False
+DOMAIN: Final = "mosoblgaz"
+DATA_CONFIG: Final = DOMAIN + "_config"
+DATA_API_OBJECTS: Final = DOMAIN + "_api_objects"
+DATA_ENTITIES: Final = DOMAIN + "_entities"
+DATA_UPDATERS: Final = DOMAIN + "_updaters"
+DATA_OPTIONS_LISTENERS: Final = DOMAIN + "_options_listeners"
 
-ATTRIBUTION = "Data provided by Mosoblgaz"
-RUB_CURRENCY = "руб."
+DEFAULT_SCAN_INTERVAL: Final = 60 * 60  # 1 hour
+DEFAULT_TIMEOUT: Final = 30  # 30 seconds
+DEFAULT_CONTRACT_NAME_FORMAT: Final = "MOG Contract {contract_code}"
+DEFAULT_METER_NAME_FORMAT: Final = "MOG Meter {meter_code}"
+DEFAULT_INVOICE_NAME_FORMAT: Final = "MOG {group} Invoice {contract_code}"
+DEFAULT_INVERT_INVOICES: Final = False
+DEFAULT_ADD_INVOICES: Final = True
+DEFAULT_ADD_METERS: Final = True
+DEFAULT_ADD_CONTRACTS: Final = True
+DEFAULT_PRIVACY_LOGGING: Final = False
 
-ENTITIES_CONTRACT = "contract"
-ENTITIES_METER_TARIFF = "meter_tariff"
+ATTRIBUTION: Final = "Data provided by Mosoblgaz"
+RUB_CURRENCY: Final = "руб."
 
-ATTR_INDICATIONS = "indications"
-ATTR_IGNORE_PERIOD = "ignore_period"
+ENTITIES_CONTRACT: Final = "contract"
+ENTITIES_METER_TARIFF: Final = "meter_tariff"
+
+ATTR_INDICATIONS: Final = "indications"
+ATTR_IGNORE_PERIOD: Final = "ignore_period"
 
 # Common attributes
-ATTR_CONTRACT_CODE = "contract_code"
-ATTR_METER_CODE = "meter_code"
+ATTR_CONTRACT_CODE: Final = "contract_code"
+ATTR_METER_CODE: Final = "meter_code"
 
 # Meter attributes
-ATTR_SERIAL = "serial"
+ATTR_SERIAL: Final = "serial"
 
 # Contract attributes
-ATTR_ADDRESS = "address"
-ATTR_PERSON = "person"
-ATTR_DEPARTMENT = "department"
+ATTR_ADDRESS: Final = "address"
+ATTR_PERSON: Final = "person"
+ATTR_DEPARTMENT: Final = "department"
 
-ATTR_COLLECTED_AT = "collected_at"
-ATTR_LAST_VALUE = "last_value"
-ATTR_LAST_COST = "last_cost"
-ATTR_LAST_CHARGED = "last_charged"
-ATTR_PREVIOUS_VALUE = "previous_value"
+ATTR_COLLECTED_AT: Final = "collected_at"
+ATTR_LAST_VALUE: Final = "last_value"
+ATTR_LAST_COST: Final = "last_cost"
+ATTR_LAST_CHARGED: Final = "last_charged"
+ATTR_PREVIOUS_VALUE: Final = "previous_value"
 
-ATTR_INVOICE_GROUP = "invoice_group"
-ATTR_PERIOD = "period"
-ATTR_TOTAL = "total"
-ATTR_PAID = "paid"
-ATTR_BALANCE = "balance"
-ATTR_PAYMENTS_COUNT = "payments_count"
+ATTR_INVOICE_GROUP: Final = "invoice_group"
+ATTR_PERIOD: Final = "period"
+ATTR_TOTAL: Final = "total"
+ATTR_PAID: Final = "paid"
+ATTR_BALANCE: Final = "balance"
+ATTR_PAYMENTS_COUNT: Final = "payments_count"
 
-ATTR_PREVIOUS_PERIOD = "previous_period"
-ATTR_PREVIOUS_TOTAL = "previous_total"
-ATTR_PREVIOUS_PAID = "previous_paid"
-ATTR_PREVIOUS_BALANCE = "previous_balance"
-ATTR_PREVIOUS_PAYMENTS_COUNT = "previous_payments_count"
+ATTR_PREVIOUS_PERIOD: Final = "previous_period"
+ATTR_PREVIOUS_TOTAL: Final = "previous_total"
+ATTR_PREVIOUS_PAID: Final = "previous_paid"
+ATTR_PREVIOUS_BALANCE: Final = "previous_balance"
+ATTR_PREVIOUS_PAYMENTS_COUNT: Final = "previous_payments_count"
 
-DEFAULT_MAX_INDICATIONS = 3
+DEFAULT_MAX_INDICATIONS: Final = 3
+ATTR_COMMENT: Final = "comment"
+ATTR_SUCCESS: Final = "success"
+ATTR_CALL_PARAMS: Final = "call_params"
+ATTR_INDICATION: Final = "indication"
+FEATURE_PUSH_INDICATIONS: Final = 1
+ATTR_IGNORE_INDICATIONS: Final = "ignore_indications"
+ATTR_INCREMENTAL: Final = "incremental"
